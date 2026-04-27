988c3ff3705460f0942b2cc6a5266a7a5f3dfd2a
ardymarky <aqmarkin@gmail.com>
2026-04-27 15:26:05 -0500

    added documentation, automated bash scripts for builds

--- simulation/torch_data_plot.m
+++ simulation/torch_data_plot.m
@@ -1,9 +1,52 @@
-close all;
+clc; clear; close all;
 
-t = sys_time_s;
-x = tfmini_range/100;
-smooth = smoothdata(x, 'gaussian',500);
-v = gradient(smooth, sys_time_s);
+file_name = "malt1_213"; % <-- change file number
+
+vars = {"sys_time_s", ...   time
+        "vms_aux0", ...     t_cmd
+        "ercf_angle", ...   angle_truth
+        "vms_aux2", ...     angle_cmd
+        "vms_aux6", ...     tar_v
+        "vms_aux7"; ...     cur_v
+        "vms_aux8", ...     tar_x
+        "vms_aux9"; ...     cur_x
+        };
+
+load("../" + file_name + ".mat", vars{:});
+
+t       = sys_time_s;
+ang_cmd = vms_aux2;
+ang     = ercf_angle;
+vx_cmd  = vms_aux6;
+vx      = vms_aux7;
+cur_x   = vms_aux8;
+tar_x   = vms_aux9;
+smooth  = smoothdata(cur_x, 'gaussian',500);
+v       = gradient(smooth, t);
+
+% X-Position
+figure()
+plot(t, cur_x * 100, 'DisplayName', 'Actual');
+hold on;
+plot(t, tar_x * 100, 'DisplayName', 'Target');
+xlabel('Time (s)');
+ylabel('X-Position (cm)');
+title('X-Position vs. Time');
+legend();
+grid on;
+
+% X-Velocity
+figure()
+plot(t, vx, 'DisplayName', 'Actual');
+hold on;
+plot(t, vx_cmd, 'DisplayName', 'Target');
+xlabel('Time (s)');
+ylabel('X-Velocity (m/s)');
+title('X-Velocity vs. Time');
+legend();
+grid on;
+
+%% Analysis
 
 threshold = 0.1;
 N = length(v);
@@ -24,7 +67,8 @@ asmooth = gradient(vfit, sys_time_s);
 figure;
 plot(smooth);
 hold on;
-plot(x);
+plot(cur_x);
+
 figure;
 plot(v);
 hold on; 
@@ -35,22 +79,21 @@ plot(a);
 hold on;
 plot(asmooth);
 
+figure;
+plot(vms_aux0(1:end)); %throttle
 
 figure;
-plot(vms_aux0(85000:end)); %throttle
-
-figure;
-plot(vms_aux1(85000:end)); %commanded v
+plot(vms_aux1(1     :end)); %commanded v
 hold on;
-plot(vms_aux2(85000:end)); %actual v ins
-
-findcoef(vms_aux0, vms_aux2, 85000, 9380,10710, Aircraft.Mass.mass_kg, Aircraft.Control.est_hover_thr)
+plot(vms_aux2(1:end)); %actual v ins
 
 
-function CD = findcoef(thrust, velocity, start, range1, range2, mass, hover)   
-    gravity = 9.81 * mass;
-    avgthrust = mean(thrust((start+range1):(start+range2))) * gravity/hover;
-    %avgvel = mean(velocity((start+range1):(start+range2)));
-    avgvel = -0.05;
+findcoef(vms_aux0, vms_aux2, tfmini_range, 1, 32509, 40648, Aircraft.Mass.mass_kg, aircraft.Control.thrust_coeff)
+
+function CD = findcoef(thrust, velocity, height, start, range1, range2, mass, hover)   
+    avgthrust = mean(thrust((start+range1):(start+range2))) * hover;
+    avgvel = mean(velocity((start+range1):(start+range2)));
+    avgheight = mean(height((start+range1):(start+range2)))/100;
+    gravity = 9.81 * mass + avgheight*.07383;
     CD = (avgthrust - gravity)/avgvel; 
 end
