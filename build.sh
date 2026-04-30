82e13ad260491906cc8fd8bddbad8a3954fa8acb
ardymarky <aqmarkin@gmail.com>
2026-04-30 12:57:25 -0500

    vel controller limiter, documentation

--- build.sh
+++ build.sh
@@ -5,14 +5,6 @@ cd simulation/
 mkdir ../flight_code/build
 cd ../flight_code/build
 cmake .. -D FMU=mini-v1 -D VEHICLE=torch -D AUTOCODE=torch_xdof
+../../tycmd.exe reset -b
 make -j6 flight_upload
-cd ../../tycmd.exe reset -b
-make -j6 flight_upload 
-
-pip3 install --user future
-if ! command -v mavproxy.py &> /dev/null; then
-    echo "Installing MAVProxy..."
-    pip3 install --user MAVProxy
-    export PATH="$PATH:$HOME/.local/bin"
-fi
-mavproxy.py --master=/dev/ttyS8 --baudrate 57600  --cmd="param load params.txt"
+make -j6 flight_upload
